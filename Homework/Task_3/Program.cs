3deda3300b6b30e5b5d1e19c55e35e1dc753034c
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-10-17 00:51:59 +0300

    Added third task. Homework done

--- Homework/Task_3/Program.cs
+++ Homework/Task_3/Program.cs
@@ -8,6 +8,37 @@
 (через запятую. использовать метод string.Split())
 */
 
-int GetNumber () {
-    
+// int GetNumber (string message) {
+//     int result = 0;
+//     bool isCorrect = false;
+//     Console.WriteLine(message);
+//     while (!isCorrect) {
+//         isCorrect = int.TryParse(Console.ReadLine(), out result);
+//         if(!isCorrect) {
+//             Console.WriteLine($"Ввели не число или оно слишком большое. Введите корректное число \n");
+//         }
+//     }
+//     return result;
+// }
+
+string GetElements(string message)
+{
+    Console.WriteLine(message);
+    string result = Console.ReadLine();
+    return result;
 }
+
+string[] GetArray(string s)
+{
+    char[] separators = new char[] { ' ', ',', '.' };
+    string[] array = s.Split(separators);
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+}
+
+
+PrintArray(GetArray(GetElements("Введите елементы массива через запятую и/или пробел: ")));
