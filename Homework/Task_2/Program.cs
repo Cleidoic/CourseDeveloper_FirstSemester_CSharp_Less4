92627ebbbbcf27f55b906b5ac5266f62ed92c1b8
Galitskiy Kirill <9keyoujob9@gmail.com>
2022-10-16 21:35:11 +0300

    Added second task

--- Homework/Task_2/Program.cs
+++ Homework/Task_2/Program.cs
@@ -6,10 +6,30 @@
 9012 -> 12
 */
 
-int GetNumber (string message) {
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    Console.WriteLine($"{message}");
+    while(!isCorrect)
+    {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
 
+        if(!isCorrect) {
+            Console.WriteLine($"Ввели не число или оно слишком большое. Введите корректное число \n");
+        }
+    }
+
+    return result;
 }
 
 int SumDigit(int num) {
-    
+    int result = 0;
+    while (num > 0) {
+        result = result + num % 10;
+        num = num /10;
+    }
+    return result;
 }
+
+Console.WriteLine(SumDigit(GetNumber("Введите число: ")));
